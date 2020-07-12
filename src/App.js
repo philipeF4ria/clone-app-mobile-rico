536d4e87a54b7c2889c20cb04921748b86298a2f
Philipe Faria <philipefaria61@gmail.com>
2020-07-12 12:06:34 -0300

    create routes directory

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Consult from './pages/Consult';
-import Invest from './pages/Invest';
-import Profile from './pages/Profile';
-
-const Tab = createBottomTabNavigator();
+import Routes from './routes';
 
 const App = () => (
 	<NavigationContainer>
-		<Tab.Navigator
-			screenOptions={({ route }) => ({
-				tabBarIcon: ({ focused, color, size }) => {
-					let iconColor;
-
-					if (route.name === 'Consult') {
-						iconColor = focused ? '#ff5300' : '#716b84';
-						return <Ionicons name="wallet-outline" size={26} color={iconColor} />
-					}
-						else if (route.name === 'Invest') {
-						iconColor = focused ? '#ff5300' : '#716b84';
-						return <MaterialCommunity name="currency-usd-circle-outline" size={26} color={iconColor} />
-					} else if (route.name === 'Profile') {
-						iconColor = focused ? '#ff5300' : '#716b84';
-						return <FontAwesome name="user-o" size={26} color={iconColor} />
-					}
-				}
-			})}
-
-			tabBarOptions={{
-				activeTintColor: '#ff5300',
-				activeBackgroundColor: '#00001d',
-				inactiveBackgroundColor: '#00001d',
-				style: {
-					height: 54,
-				},
-				labelStyle: {
-					fontSize: 14
-				}
-			}}
-		>
-			<Tab.Screen name="Consult" component={Consult} options={{ tabBarLabel: 'Consultar' }}/>
-			<Tab.Screen name="Invest" component={Invest} options={{ tabBarLabel: 'Investir' }}/>
-			<Tab.Screen name="Profile" component={Profile} options={{ tabBarLabel: 'Perfil' }}/>
-		</Tab.Navigator>
+		<Routes />
 	</NavigationContainer>
 );
 
